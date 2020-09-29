65a951faf93e9b0232436d1eb174738b173506e1
Jared Storm <jaredrstorm@gmail.com>
2020-09-28 22:45:40 -0400

    Add ScrollView

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.row}>
-        <Image style={styles.assetImage} source={require('./assets/8MM.png')} />
-        <Image style={styles.assetImage} source={require('./assets/CaliforniaLut.png')} />
-      </View>
-      <View style={styles.row}>
-        <Image style={styles.assetImage} source={require('./assets/FilmBurns.png')} />
-        <Image style={styles.assetImage} source={require('./assets/GrainPack.png')} />
-      </View>
-      <View style={styles.row}>
-        <Image style={styles.assetImage} source={require('./assets/TapeDamage.png')} />
-        <Image style={styles.assetImage} source={require('./assets/TextOverlays.png')} />
-      </View>
-      {/* <View style={styles.row}>
-        <Image style={styles.assetImage} source={require('./assets/Viewfinder.png')} />
-      </View> */}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.row}>
+          <Image style={styles.assetImage} source={require('./assets/8MM.png')} />
+          <Image style={styles.assetImage} source={require('./assets/CaliforniaLut.png')} />
+        </View>
+        <View style={styles.row}>
+          <Image style={styles.assetImage} source={require('./assets/FilmBurns.png')} />
+          <Image style={styles.assetImage} source={require('./assets/GrainPack.png')} />
+        </View>
+        <View style={styles.row}>
+          <Image style={styles.assetImage} source={require('./assets/TapeDamage.png')} />
+          <Image style={styles.assetImage} source={require('./assets/TextOverlays.png')} />
+        </View>
+        <View style={styles.row}>
+          <Image style={styles.assetImage} source={require('./assets/Viewfinder.png')} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -38,6 +40,7 @@ const styles = StyleSheet.create({
   assetImage: {
     height: 180,
     width: 120,
-    marginHorizontal: 20
+    marginHorizontal: 20,
+    marginVertical: 10,
   }
 });
