ee5cfe7fc57ed319565f931fbe91a892d5574162
Jared Storm <jaredrstorm@gmail.com>
2020-10-05 21:22:49 -0400

    Cleanup imports

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView } from 'react-native';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
